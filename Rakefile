ddc6de66f32efe66f802c2f5609fe5577c292e38
AJ Christensen <aj@junglist.gen.nz>
2012-01-29 21:50:38 +1300

    switch to rspec

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require "rubygems"
+require "rubygems" unless RUBY_VERSION =~ /1.9/
 require "bundler"
 begin
   Bundler.setup(:default, :development)
--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,4 +1,12 @@
 require "rubygems" unless RUBY_VERSION =~ /1.9/
 
-require "minitest/spec"
-require "minitest/autorun"
+require "bundler"
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
+require_relative "../lib/pylon"
